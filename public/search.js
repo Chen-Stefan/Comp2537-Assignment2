ba6e1eabc5a42429b4d1ee518d701e5e9c39c24d
Stefan Chen <zchen158@my.bcit.ca>
2022-05-13 01:01:13 -0700

    Update function in search.js to improve timeline hit numbering

--- public/search.js
+++ public/search.js
@@ -2,7 +2,10 @@ let currentType = null;
 let currentRegion = null;
 let pokemonName = null;
 let nameInput = null;
-let typeidObject = {};
+let typeArray = [];
+let typehitObject = {};
+let regionHit = 1;
+let nameHit = 1;
 let regionidObject = {};
 let now = new Date(Date.now());
 let formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
@@ -119,7 +122,9 @@ function clearHistory() {
 }
 
 function addNewTypeTimelineEvent(pokemonType) {
-    // if (!(pokemonType in typeidObject)) {
+    if (!(typeArray.includes(pokemonType))) {
+        typeArray.push(pokemonType);
+        typehitObject[`${pokemonType}`] = 1;
         $.ajax({
             type: "PUT",
             url: "http://localhost:5000/timeline/insert",
@@ -128,18 +133,31 @@ function addNewTypeTimelineEvent(pokemonType) {
                 hits: 1,
                 time: `${now}`
             },
-            success: (res) => { console.log("We have a new type search")
-                // typeidObject[`${pokemonType}`] = res._id;
+            success: (res) => { 
+                console.log(`We have a total of 1 search for type ${pokemonType}`)
+                // console.log(typeArray)
             }
         })
-    // }else{
-    //     let id = typeidObject[`${pokemonType}`];
-    //     $.ajax({
-    //         type: "GET",
-    //         url: `http://localhost:5000/timeline/incrementHits/${id}`,
-    //         success: (res) => {console.log(res)}
-    //     })
-    // }
+    }else{
+        $.ajax({
+            type: "PUT",
+            url: "http://localhost:5000/timeline/insert",
+            data: {
+                text: `User searched for pokemon type: ${pokemonType}`,
+                hits: typehitObject[`${pokemonType}`],
+                time: `${now}`
+            },
+            success: (res) => { 
+                console.log(`We have a total of ${typehitObject[`${pokemonType}`] + 1} search for type ${pokemonType}`)
+                let id = res._id;
+                $.ajax({
+                    type: "GET",
+                    url: `http://localhost:5000/timeline/incrementHits/${id}`,
+                    success: (res) => {typehitObject[`${pokemonType}`] ++;}
+                })
+            }
+        })
+    }
 }
 
 function addNewRegionTimelineEvent(currentRegion) {
